1f2bdb6c261b0582f4d355dc8f4e30152633eb7e
JURI <80569323+juri123123@users.noreply.github.com>
2024-07-26 00:40:39 +0900

    project init

---
+++ WidgetPractice/WidgetPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPracticeApp.swift
+//  WidgetPractice
+//
+//  Created by 최주리 on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
